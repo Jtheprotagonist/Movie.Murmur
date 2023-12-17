266bd6b35270a176a3492ba4052cc17d44c49fee
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2023-12-17 21:19:57 +0900

    Updated misc

--- index.js
+++ index.js
@@ -2,106 +2,105 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { Movie, User, Users } = Models;
+const Movies = Models.Movie;
+const { check, validationResult } = require('express-validator');
+const config = require('./config');
+const saltRounds = 10;
 
-const { Movie, User } = Models; // Destructuring assignment for better readability
-
-const connectOptions = {
+mongoose.connect('mongodb://localhost:27017/moviedb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-};
-
-mongoose.connect('mongodb://localhost:27017/moviedb', connectOptions)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+})
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cors = require('cors');
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-
-// Gets the list of all movies
-
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  await Movie.find()
-    .then((movie) => {
-      res.status(201).json(movie);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
+app.get('/', (req, res) => {
+  res.send('Welcome to the best movie search app ever!(Maybe😁)');
 });
 
-// Gets the data about a single movie, by title
+// applies the jwt authentication to every route, except register 
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  await Movies.find()
+      .then((movies) => {
+          res.status(201).json(movies);
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(400).send('An Error occurred: ' + err);
+      })
+});
 
-app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const movie = movies.find((m) => m.title === req.params.title);
-  if (movie) {
-    res.json(movie);
-  } else {
-    res.status(404).send('Movie not found.');
+// Add a user
+app.post('/users',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+      .then((user) => {
+        if (user) {
+          //If the user is found, send a response that it already exists
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) => { res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
+
+// Get all users
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
   }
 });
 
-
-app.get('/users', passport.authenticate('jwt', { session: false }), function (req, res) {
-  User.find()
-    .then(function (user) {
-      res.status(200).json(user);
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
-
-
-
-
-//Add a user
-/* We’ll expect JSON in this format
-{
-  ID: Integer,
-  Username: String,
-  Password: String,
-  Email: String,
-  Birthday: Date
-}*/
-app.post('/users', async (req, res) => {
-  await User.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        User
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
-});
 
 // Get a user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -120,69 +119,71 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), as
 
 
 // Update a user's info, by username
-/* We’ll expect JSON in this format
-{
-  Username: String,
-  (required)
-  Password: String,
-  (required)
-  Email: String,
-  (required)
-  Birthday: Date
-}*/
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
+  // Validation logic here for request
+  // you can either use a chain of methods like .not().isEmpty()
+  // which means "opposite of isEmpty" in plain English "is not empty"
+  // or use .isLength({min: 5}) which means
+  // minimum value of 5 characters is only allowed
+  check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+  // check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   // CONDITION TO CHECK ADDED HERE
-  if(req.user.Username !== req.params.Username){
-      return res.status(400).send('Permission denied');
+  if (req.user.Username !== req.params.Username) {
+    return res.status(400).send('Permission denied');
   }
   // CONDITION ENDS
+
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
-      $set:
-      {
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-      }
+    $set: {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
   },
-      { new: true }) // This line makes sure that the updated document is returned
-      .then((updatedUser) => {
-          res.json(updatedUser);
-      })
-      .catch((err) => {
-          console.log(err);
-          res.status(500).send('Error: ' + err);
-      })
+  { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
+
+
 
 
 // Add a movie to a user's list of favorites
-app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  try {
-    const { Username, MovieID } = req.body;
-
-    if (!Username || !MovieID) {
-      return res.status(400).send('Username and MovieID must be provided in the request body.');
-    }
-
-    const updatedUser = await User.findOneAndUpdate(
-      { Username: Username },
-      { $push: { FavoriteMovies: MovieID } },
-      { new: true }
-    );
-
-    if (updatedUser) {
-      res.json(updatedUser);
-    } else {
-      res.status(404).send('User not found.');
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
+app.put('/users/:Username/movies/add/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  if (req.user.Username !== req.params.Username) {
+      return res.status(400).send('Permission denied!')
   }
+
+  await Users.findOneAndUpdate({ Username: req.params.Username },
+      { $push: { FavoriteMovies: req.params.MovieID } },
+      { new: true })
+      .then((updatedUser) => {
+          res.status(201).send('Successfully added the movie to the favorite List!\n' + JSON.stringify({
+              Username: updatedUser.Username,
+              FavoriteMovies: updatedUser.FavoriteMovies,
+          }, null, 2));
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(400).send('Couldn\t add movie to favorites List-Err: ' + err);
+      })
 });
-
-
 
 // Delete a user by username
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
@@ -17,15 +18,27 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model("Movie", movieSchema);
 let User = mongoose.model("User", userSchema);
+let Users = mongoose.model("Users", userSchema); // Add this line
 
-module.exports.Movie = Movie;
-module.exports.User = User;
+module.exports = {
+  Movie,
+  User,
+  Users, // Add this line
+};
