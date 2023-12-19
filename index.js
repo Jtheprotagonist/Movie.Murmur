5ee15c7d26549bb623dfaa09911e1c90324d0f91
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2023-12-19 09:48:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -202,9 +202,11 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 });
 
 
-app.listen(8081, () => {
-  console.log('Your app is listening on port 8081');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
+
 
 
 
