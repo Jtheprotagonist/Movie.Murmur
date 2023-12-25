9794a0ab47b45dbbb5f65c51c4b4792ccefb1a21
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2023-12-25 11:13:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const { check, validationResult } = require('express-validator');
 const config = require('./config');
 const saltRounds = 10;
 
-mongoose.connect('mongodb://localhost:27017/moviedb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// mongoose.connect('mongodb://localhost:27017/moviedb', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+
+mongoose.connect(process.env.CONNECTION_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
 })
 
 const app = express();
