452c38491f4e753b056e630dd372572d251884b0
Jtheprotagonist <137086687+Jtheprotagonist@users.noreply.github.com>
2024-03-27 09:42:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,53 @@
-const mongoose = require('mongoose');
-const Models = require('./models.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const { Movie, User, Users } = Models;
-const Movies = Models.Movie;
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
+const Models = require('./models.js');
 const config = require('./config');
-const saltRounds = 10;
-
-mongoose.connect(process.env.CONNECTION_URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-})
+const { Movie, User } = Models;
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const cors = require('cors');
-app.use(cors());
-
-let auth = require('./auth')(app);
-const passport = require('passport');
-require('./passport');
-
-// Import dependencies for JWT authentication
-const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt');
-
-// Configure Passport to use JWT authentication
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.secret,
-}, (jwtPayload, done) => {
-  Users.findById(jwtPayload._id)
-    .then(user => {
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    })
-    .catch(err => done(err, false));
-}));
-
-// Protected movies endpoint using Passport authentication middleware
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
-  await Movies.find()
-      .then((movies) => {
-          res.status(200).json(movies);
-      })
-      .catch((err) => {
-          console.error(err);
-          res.status(500).send('An Error occurred: ' + err);
-      })
+// MongoDB connection
+mongoose.connect(process.env.CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-// Add more routes as needed
+// Movies endpoint
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.status(200).json(movies);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An Error occurred: ' + err);
+    }
+});
+
+// Login endpoint
+app.post('/login', [
+    check('username', 'Username is required').notEmpty(),
+    check('password', 'Password is required').notEmpty()
+], async (req, res) => {
+    // Code for login endpoint
+});
+
+// Signup endpoint
+app.post('/signup', [
+    check('username', 'Username is required').notEmpty(),
+    check('password', 'Password is required').notEmpty()
+], async (req, res) => {
+    // Code for signup endpoint
+});
+
+// Other endpoints...
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, '0.0.0.0', () => {
+    console.log('Listening on Port ' + port);
 });
